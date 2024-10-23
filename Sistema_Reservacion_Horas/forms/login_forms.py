086e94d6378c96e3913ee60cf630f7d5b049765a
eddm2417 <eddm2417@gmail.com>
2024-10-23 17:26:39 -0400

    Validaciones de Seguridad Login

--- Sistema_Reservacion_Horas/forms/login_forms.py
+++ Sistema_Reservacion_Horas/forms/login_forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from Sistema_Reservacion_Horas.models.aulas_model import Usuario
 
+
 class LoginForm(forms.Form):
     no_carnet = forms.CharField(
         label='Número de Carnet',
@@ -12,21 +13,22 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
 
-    def clean(self):
-        cleaned_data = super().clean()
-        no_carnet = cleaned_data.get("no_carnet")
-        password = cleaned_data.get("password")
-
-        # Verifica si los campos están vacíos
-        if not no_carnet or not password:
-            raise forms.ValidationError("Por favor, complete todos los campos.")
-
-        # Verifica si el usuario existe y la contraseña es correcta
-        try:
-            usuario = Usuario.objects.get(no_carnet=no_carnet)  # Busca por número de carnet
-            if usuario.password != password:  # Considera usar un método seguro para comparar contraseñas
-                raise forms.ValidationError("Contraseña incorrecta.")
-        except Usuario.DoesNotExist:
+    def clean_no_carnet(self):
+        no_carnet = self.cleaned_data.get("no_carnet")
+        # Verificar si el usuario existe
+        if not Usuario.objects.filter(no_carnet=no_carnet).exists():
             raise forms.ValidationError("El usuario no está registrado.")
+        return no_carnet
 
-        return cleaned_data
+    def clean_password(self):
+        no_carnet = self.cleaned_data.get("no_carnet")
+        password = self.cleaned_data.get("password")
+
+        if no_carnet and password:
+            try:
+                usuario = Usuario.objects.get(no_carnet=no_carnet)
+                if usuario.password != password:  # Verificación básica de contraseñas
+                    raise forms.ValidationError("Contraseña incorrecta.")
+            except Usuario.DoesNotExist:
+                pass  # Esto ya lo manejamos en clean_no_carnet
+        return password
